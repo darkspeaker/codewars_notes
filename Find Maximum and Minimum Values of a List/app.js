8197830274340447e63925b559757b6d38ca25e0
darkspeaker <graggg13@gmail.com>
2022-11-19 20:27:59 +0200

    feat: min max kata

---
+++ Find Maximum and Minimum Values of a List/app.js
@@ -0,0 +1,12 @@
+var min = function(list){
+    
+  const res = Math.min(...list)
+  return res
+}
+
+var max = function(list){
+  const res = Math.max(...list)
+  return res
+}
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110]))
